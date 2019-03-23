7544f291bea3534b83f7f8c670109ff00507ee0a
Zeidat <mahmoud.zeidat@iconnecths.com>
2019-03-23 18:45:46 +0300

    add custom edit text to login activity

--- PalMenu/app/src/main/java/com/zeidat/palmenu/activites/Login.java
+++ PalMenu/app/src/main/java/com/zeidat/palmenu/activites/Login.java
@@ -2,7 +2,6 @@ package com.zeidat.palmenu.activites;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.zeidat.palmenu.MainActivity;
 import com.zeidat.palmenu.R ;
 
 public class Login extends AppCompatActivity {
