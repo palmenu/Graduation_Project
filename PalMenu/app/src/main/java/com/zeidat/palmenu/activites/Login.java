a4281c3579ccbb3ff29b3b443e4f5a5a798ac534
Zeidat <mahmoud.zeidat@iconnecths.com>
2019-03-23 18:35:29 +0300

    add login activity

---
+++ PalMenu/app/src/main/java/com/zeidat/palmenu/activites/Login.java
@@ -0,0 +1,15 @@
+package com.zeidat.palmenu.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.zeidat.palmenu.MainActivity;
+import com.zeidat.palmenu.R ;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
